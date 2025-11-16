624a23f14bed37a4a8e8fcd079372835f2e7dd44
Pankaj Kumar Agarwal <pankaj.7613@gmail.com>
2025-11-16 15:50:57 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ const { default: connectDB } = await import('./db/index.js');
 await connectDB();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log('Server is running on port:', PORT);
+// app.listen(PORT, () => {
+//     console.log('Server is running on port:', PORT);
+// });
+
+app.listen(3002, "0.0.0.0", () => {
+  console.log("Server running on port 3002");
 });
