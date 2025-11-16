e898cdc10e4f23173f060360751a11b1bf007b2e
Pankaj Kumar Agarwal <pankaj.7613@gmail.com>
2025-11-16 15:30:40 +0530

    fix

--- src/db/index.js
+++ src/db/index.js
@@ -12,13 +12,16 @@ import mysql from 'mysql2/promise';
 // };
 
 // --- env (already loaded in index.js) ---
-const DB_HOST = process.env.DB_HOST || 'localhost';  // GoDaddy: 'localhost'; local can be '127.0.0.1'
-const DB_PORT = Number(process.env.DB_PORT || 3306);
-const DB_NAME = process.env.DB_NAME || 'whatsappTool';
-const DB_USER = process.env.DB_USER ?? 'pankaj_tinku';
-const DB_PASSWORD = process.env.DB_PASSWORD ?? 'Pankaj12345$$';
+console.log("DB_HOST : ",process.env.DB_HOST);
+//Prod
+const DB_HOST = 'ls-322b13a438adc6602193777abc66dd7f1b391154.cd0w8y80q80x.ap-south-1.rds.amazonaws.com';  // GoDaddy: 'localhost'; local can be '127.0.0.1'
+const DB_PORT = Number(3306);
+const DB_NAME = 'saas_whatsapp_manager';
+const DB_USER = 'dbmasteruser';
+const DB_PASSWORD = '&s0rpr,(G2+1sv7rT3MGKa{c:-j<ebN:';
 const CREATE_DB_IF_MISSING = (process.env.CREATE_DB_IF_MISSING || 'false') === 'true';
 const SYNC_DB = (process.env.SYNC_DB || 'false') === 'true';
+
 
 // --- sequelize instance ---
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
